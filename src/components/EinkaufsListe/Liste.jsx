5ec738aa884186179263ee1ccccc7986fa080e1a
Dima Linker <daniel.linker@mailbox.org>
2024-03-25 15:15:51 +0100

    React icons

--- src/components/EinkaufsListe/Liste.jsx
+++ src/components/EinkaufsListe/Liste.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Liste.scss'
+import {RiDeleteBin6Line} from "react-icons/ri";
 
 export const Liste = ({listen}) => {
     return (
         <div className="liste">
             {listen.text}
+            <RiDeleteBin6Line className="deleteIcon" />
         </div>
     )
 }
